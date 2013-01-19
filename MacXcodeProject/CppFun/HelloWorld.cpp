2ed71e9bc4d8af7441170bb209cf20af8559a0d2
Brian Abbott <Brian@Brians-MacBook-Air.local>
2013-01-18 19:12:45 -0800

    adding base project

---
+++ MacXcodeProject/CppFun/HelloWorld.cpp
@@ -0,0 +1,22 @@
+//
+//  HelloWorld.cpp
+//  CppFun
+//
+//  Created by Brian Abbott on 1/16/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::string s = "string 1";
+    std::string s1("string 2");
+//    std::string* s2 = new std::string();
+    
+    std::cout << s << std::endl;
+    std::cout << s1 << std::endl;
+//    std::cout << s2 << std::endl;
+    
+    std::cout << "hello world!!!!!!" << std::endl;
+}
